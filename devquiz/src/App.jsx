a881d60f696006b51040e8cb6a715402ecca3953
Lucas Quadros <olucasquadros@gmail.com>
2022-10-26 21:48:19 -0300

    second

--- devquiz/src/App.jsx
+++ devquiz/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Welcome from './components/Welcome'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
   )
 }
